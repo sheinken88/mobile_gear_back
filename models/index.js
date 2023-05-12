fef76e83893d93b6cae4f4f7d37c529ab44b51ae
JuanVelazquez87 <velazquezjuancarlos87@gmail.com>
2023-05-12 17:47:34 -0300

    sincronizacion db con modelos

--- models/index.js
+++ models/index.js
@@ -2,8 +2,12 @@ const Users = require("./Users");
 const Products = require("./Products");
 const Brands = require("./Brands");
 const Categories = require("./Categories");
+const Order = require("./Order");
 
 Products.belongsTo(Brands);
 Products.belongsTo(Categories);
-
-module.exports = { Users, Products, Brands, Categories };
+Order.belongsTo(Users);
+Users.hasMany(Order);
+Order.belongsToMany(Products, { through: "orderproducts" });
+Products.belongsToMany(Order, { through: "orderproducts" });
+module.exports = { Users, Products, Brands, Categories, Order };
