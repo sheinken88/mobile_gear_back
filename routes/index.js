95620ea3168113bcb5f3579d2db477c6c0cd69fc
alba-97 <alvarezbasilio97@gmail.com>
2023-05-23 22:39:11 -0300

    se elimina cart que no se usa

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,11 @@ const router = express.Router();
 const users = require("./users");
 const products = require("./products");
 const admin = require("./admin");
-const cart = require("./cart");
 const orders = require("./orders");
 const categories = require("./categories");
 
 router.use("/users", users);
 router.use("/products", products);
-router.use("/cart", cart);
 router.use("/admin", admin);
 router.use("/orders", orders);
 router.use("/categories", categories);
