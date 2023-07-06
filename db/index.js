360b6e7873eccaf59c485f7483408a479cbee5e0
PC <heinken.sebastian@gmail.com>
2023-07-06 15:41:51 -0300

    deploy settings

--- db/index.js
+++ db/index.js
@@ -11,14 +11,9 @@
 const Sequelize = require("sequelize");
 require("dotenv").config({ path: ".env.development.local" });
 
-const db = new Sequelize(
-  process.env.POSTGRES_URL,
-  process.env.POSTGRES_USER,
-  process.env.POSTGRES_PASSWORD,
-  {
-    dialectModule: require("pg"),
-    logging: false,
-  }
-);
+const db = new Sequelize(process.env.POSTGRES_URL, {
+  dialectModule: require("pg"),
+  logging: false,
+});
 
 module.exports = db;
